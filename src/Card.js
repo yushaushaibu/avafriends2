e4b7b6eb6b4676bcf9b459e9bd53d8e989554ecc
yushaushaibu <yushaushaibu1@gmail.com>
2023-01-10 22:09:53 +0000

    Created Dynamic Component

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ id, name, email }) => {
     return  (
-        <div>
-            <img alt='robots' src='https://robohash.org/tuesday?200*200' />
+        <div className='bg-light-green dib br3 pa2 ma2 grow bw2 shadow-5'>
+            <img alt='robots' src={`https://robohash.org/${id}?200*200`} />
             <div>
-                <h2>Jane Doe</h2>
-                <p>jane.doe@gmail.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -5,4 +5,11 @@ import './index.css';
 import tachyons from 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Card />);
+root.render(
+    <div>
+        <Card id="0" name="Rose" email="d.rose@nba.com"/>
+        <Card id="1" name="Wall" email="j.wall@nba.com"/>
+        <Card id="2" name="Irving" email="k.irving@nba.com"/>
+    </div>
+
+);
