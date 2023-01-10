9935613fd1d9a9904ca5f88bb77dc76872a45d5d
yushaushaibu <yushaushaibu1@gmail.com>
2023-01-10 21:52:55 +0000

    Card component created

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Card from './Card';
+import './index.css';
+import tachyons from 'tachyons';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Card />);
