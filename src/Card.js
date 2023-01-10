07690df5d57773c77937b2305a2c7a5314cb419a
yushaushaibu <yushaushaibu1@gmail.com>
2023-01-10 23:30:14 +0000

    Robots Components Rendering

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Card = ({ id, name, email }) => {
-    return  (
+const Card = ({ name, email, id }) => {
+    return (
         <div className='bg-light-green dib br3 pa2 ma2 grow bw2 shadow-5'>
             <img alt='robots' src={`https://robohash.org/${id}?200*200`} />
             <div>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Card from './Card';
-import './index.css';
-import tachyons from 'tachyons';
+import CardList from "./CardList";
+import "./index.css";
+import tachyons from "tachyons";
+import { robots } from "./robots";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <div>
-        <Card id="0" name="Rose" email="d.rose@nba.com"/>
-        <Card id="1" name="Wall" email="j.wall@nba.com"/>
-        <Card id="2" name="Irving" email="k.irving@nba.com"/>
-    </div>
-
-);
+root.render(<CardList robots={robots} />);
