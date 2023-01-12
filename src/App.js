1813796d5b0ea53d4e2a1a5908331f751294422e
yushaushaibu <yushaushaibu1@gmail.com>
2023-01-12 12:41:50 +0000

    fetch user data using componentDidMount function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
 import './App.css'
 
 class App extends Component {
   state = {
-    robots: robots,
+    robots: [],
     searchfield: "",
   };
+
+  componentDidMount() {
+     fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(user => {
+        this.setState({ robots: user })
+      })
+      .catch(err => console.log(err))
+  }
 
   searchChangeHandler = (event) => {
     this.setState({ searchfield: event.target.value });
@@ -20,13 +28,18 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    return (
-      <div className="tc">
-        <h1>AvafriendsII</h1>
-        <SearchBox searchChange={this.searchChangeHandler} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    
+    if (this.state.robots.length === 0) {
+      return <h1 className="light-red">Loading..</h1>
+    } else {
+      return (
+        <div className="tc">
+          <h1>AvafriendsII</h1>
+          <SearchBox searchChange={this.searchChangeHandler} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 
